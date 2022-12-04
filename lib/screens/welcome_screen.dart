2b5d20c340b3c9c64baf375295ff2eb5297ae7ed
MarcelliaNr <marcellianr1234@gmail.com>
2022-12-04 13:32:06 +0700

    welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -12,83 +12,69 @@ class GetStartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              "assets/icons/urban.jpg",
-            ),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: SafeArea(
+      body: Center(
+        child: Container(
           child: Column(
-            children: [
-              /// title
-              Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: Text(
-                    "We Provide High Quality Products Just for You",
-                    style: GoogleFonts.notoSansChakma(
-                      fontSize: 50,
-                      color: Colors.white,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(35),
+                height: 220,
+                width: 400,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "assets/icons/getstar.jpg",
                     ),
-                  )),
-              const Spacer(),
-
-              /// button to navigate to home screen`
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SignInScreen(),
-                      ));
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(48),
                   ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 32,
-                    vertical: 24,
-                  ),
-                ),
-                child: Text(
-                  "GET STARTED",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium
-                      ?.copyWith(color: ThemeColor.black),
                 ),
               ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an Account ?",
-                    style: Theme.of(context)
-                        .textTheme
-                        .button
-                        ?.copyWith(color: ThemeColor.white),
-                  ),
-                  const SizedBox(width: 8),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Login Here !",
-                      style: Theme.of(context)
-                          .textTheme
-                          .button
-                          ?.copyWith(color: ThemeColor.white),
+              Expanded(
+                  child: Container(
+                child: SafeArea(
+                    child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(2),
+                      child: Text(
+                        "Discover Your Own Dream House",
+                        style: GoogleFonts.notoSansChakma(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              )
+                    Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignInScreen(),
+                            ));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 22,
+                        ),
+                      ),
+                      child: Text(
+                        "Get Started",
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(color: ThemeColor.black),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                  ],
+                )),
+              ))
             ],
           ),
         ),
